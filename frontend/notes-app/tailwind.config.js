1b79d1f7b16e53962ba23fbcf72dcd5b186ae8d5
Kiwirish <blakeleahy7@gmail.com>
2025-01-13 20:20:12 +1300

    Login page setup with password view toggle working, and nice UI

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      //colours used in project
+      colors: {
+        primary: "#2B85FF",
+        seconary: "EF863E",
+      },
+    },
   },
   plugins: [],
 }
