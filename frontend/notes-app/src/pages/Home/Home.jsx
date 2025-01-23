a13e9d3b886ec0db239fcb270b2a66c5be7032db
Kiwirish <blakeleahy7@gmail.com>
2025-01-23 15:27:46 +1300

    Modal for adding note added and working

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from "../../components/Navbar/Navbar";
 //import ProfileInfo from "../../components/cards/ProfileInfo";
 import NoteCard from '/Users/blakeleahy/Desktop/MERN-Note-App/frontend/notes-app/src/components/cards/NoteCard.jsx';
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdOutlineAlarmAdd } from "react-icons/md";
 import AddEditNotes from '/Users/blakeleahy/Desktop/MERN-Note-App/frontend/notes-app/src/pages/Home/AddEditNotes.jsx';
+import Modal from "react-modal";
+
 
 const Home = () => {
+
+    const [openAddEditModal, setOpenAddEditModal] = useState({
+        isShown: false,
+        type: "add",
+        data: null,
+
+    });
+
+
     return (
        <>
        <Navbar />
@@ -25,11 +36,31 @@ const Home = () => {
        </div>
 
         <button className="w-16 h-16 items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
-            onClick={() => {}}>
+            onClick={() => {
+                setOpenAddEditModal({ isShown: true, type: "add", data: null});
+            }}
+        >
             <MdAdd className="text-[32px] text-white" />
         </button>
 
-        < AddEditNotes />
+        <Modal
+            isOpen={openAddEditModal.isShown}
+            onRequestClose={()=>{}}
+            style={{
+                overlay: {
+                    backgroundColor: "rgba(0,0,0,0.2)",
+                },
+            }}
+            contentLabel=""
+            className=" w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll
+            "
+        >
+{/* w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll
+ */} 
+
+ 
+            < AddEditNotes />
+        </Modal>
        </>
     )
 }
