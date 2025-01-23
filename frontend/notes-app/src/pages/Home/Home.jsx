966559f2e5c462d3b7f3545a34961405ddf7fdb5
Kiwirish <blakeleahy7@gmail.com>
2025-01-23 14:56:44 +1300

    Add new note done WITHOUT modal yet..

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Navbar from "../../components/Navbar/Navbar";
 //import ProfileInfo from "../../components/cards/ProfileInfo";
 import NoteCard from '/Users/blakeleahy/Desktop/MERN-Note-App/frontend/notes-app/src/components/cards/NoteCard.jsx';
-import { MdAdd } from "react-icons/md"
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from '/Users/blakeleahy/Desktop/MERN-Note-App/frontend/notes-app/src/pages/Home/AddEditNotes.jsx';
+
 const Home = () => {
     return (
        <>
@@ -22,11 +24,12 @@ const Home = () => {
             </div>
        </div>
 
-        <button className="w-16 h-16 items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <button className="w-16 h-16 items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+            onClick={() => {}}>
             <MdAdd className="text-[32px] text-white" />
         </button>
 
-
+        < AddEditNotes />
        </>
     )
 }
